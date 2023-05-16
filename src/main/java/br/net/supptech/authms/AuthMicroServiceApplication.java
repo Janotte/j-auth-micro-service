1a2b4667863d66bfc466be3ccad989784e8f45c6
Sandro André Janotte <sjanotte@gmail.com>
2023-05-15 22:09:29 -0300

    Initial commit

---
+++ src/main/java/br/net/supptech/authms/AuthMicroServiceApplication.java
@@ -0,0 +1,13 @@
+package br.net.supptech.authms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthMicroServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthMicroServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/net/supptech/authms/AuthMicroServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.net.supptech.authms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthMicroServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
